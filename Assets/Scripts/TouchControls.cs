37b88148d507cb3756d67ba0441db866844f67e4
alexanderchatham <alexchatham@Alexs-MacBook-Pro.local>
2018-01-11 14:20:17 -0500

    Touch controls started

---
+++ Assets/Scripts/TouchControls.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchControls : MonoBehaviour {
+
+	bool left = false;
+	bool right = false;
+
+	void Start(){
+	
+		Rect jumpRect = new Rect (0,Screen.height/2,Screen.width/2, Screen.height/2);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (Touch touch in Input.touches)
+		{
+			if (touc)
+		}
+	}
+}
