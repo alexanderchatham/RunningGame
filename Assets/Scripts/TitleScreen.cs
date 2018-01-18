dc6ef850069e1ff7bf8c7b798fdc0fea25c96336
Alex <chatham.alexander@gmail.com>
2018-01-18 04:57:23 -0500

    Title screen
    also updated jump animations

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreen : MonoBehaviour {
+
+    public GameObject panel;
+
+    public void hide()
+    {
+        panel.SetActive(false);
+    }
+}
