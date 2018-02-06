d571692874f5f71d0aea39d89dbc9cb5839b85e9
alexanderchatham <alexchatham@Alexs-MacBook-Pro.local>
2018-02-06 18:33:25 -0500

    started lvl 12

---
+++ Assets/DisappearingPlatform.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisappearingPlatform : MonoBehaviour {
+
+	BoxCollider2D plank;
+	// Use this for initialization
+	void Start () {
+		plank = GetComponentInChildren<BoxCollider2D> ();
+	}
+	
+	void OnCollisionEnter2D(){
+		
+	}
+}
