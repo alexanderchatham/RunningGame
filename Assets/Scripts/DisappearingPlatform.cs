d2251ff0357a3f15fb654d5cec58fa61cfec0c50
Alex <chatham.alexander@gmail.com>
2018-02-07 17:49:31 -0500

    Added levels 12 13start
    added disappearing platforms and new prefabs

---
+++ Assets/Scripts/DisappearingPlatform.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisappearingPlatform : MonoBehaviour {
+
+	BoxCollider2D plank;
+    Animator anim;
+	// Use this for initialization
+	void Start () {
+		plank = GetComponent<BoxCollider2D> ();
+        anim = GetComponent<Animator>();
+    }
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        anim.SetBool("disappear",true);
+    }
+
+    public void drop()
+    {
+        Destroy(plank);
+    }
+
+    public void end()
+    {
+        Destroy(this.transform.parent.gameObject);
+    }
+}
