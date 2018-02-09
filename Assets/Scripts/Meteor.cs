026eb4f3498614303c9f0774c44eb35b12f12854
Alex <chatham.alexander@gmail.com>
2018-02-09 16:14:28 -0500

    Now there are 17 levels
    added buttons for up to level 20

---
+++ Assets/Scripts/Meteor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteor : MonoBehaviour {
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+    public float angle = 90;
+    public float pause = 0;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        rb.gravityScale = 0f;
+        rb.transform.eulerAngles = new Vector3(0,0,angle);
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (pause > 0)
+        {
+            pause -= Time.deltaTime;
+            return;
+        }
+        rb.transform.Translate(0, GameMaster.characterMoveSpeed, 0);
+	}
+}
