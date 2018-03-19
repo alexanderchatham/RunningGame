c4e0de66c29d6bd7c85915718388186070d695f3
alexanderchatham <alexchatham@Alexs-MacBook-Pro.local>
2018-03-19 19:47:28 -0400

    ready for store

--- Assets/Scripts/Group.cs
+++ Assets/Scripts/Group.cs
@@ -5,27 +5,9 @@ using UnityEngine;
 public class Group : MonoBehaviour {
 
 	public GameObject ground;
-	private GameObject startPoint;
-	private GameObject endPoint;
-	private bool tileSpawned;
-	private GameObject newGround;
 
-	// Update is called once per frame
-	private void Start()
-	{
-
-		endPoint = GameObject.FindGameObjectWithTag("End");
-
-		startPoint = GameObject.FindGameObjectWithTag("Start");
-
-
-		//coroutines for movement
-	}
 	void FixedUpdate () {
-		if(ground.transform.position.x <= endPoint.transform.position.x)
-		{
-			Destroy(ground);
-		}
-		this.transform.Translate(GameMaster.groundMoveSpeed,0,0);
+		
+		ground.transform.Translate(GameMaster.groundMoveSpeed,0,0);
 	}
 }
